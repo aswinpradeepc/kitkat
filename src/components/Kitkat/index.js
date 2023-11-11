d35f58ce69051e06649fd131e29f85be1b73b373
aswinpradeepc <aswinpradeepc@gmail.com>
2023-11-11 20:36:48 +0530

    initial commit

---
+++ src/components/Kitkat/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const Kitkat = () => {
+    const [tapCount, setTapCount] = useState(0);
+
+    const handleTap = () => {
+        setTapCount(tapCount + 1);
+    };
+
+    const divStyle = {
+        width: '100%',
+        height: '100vh',
+        backgroundColor: 'red',
+        cursor: 'pointer',
+    };
+
+    return (
+        <div >
+            <div>
+                <h1>Kitkat Taps: {tapCount}</h1>
+            </div>
+            <div style={divStyle} onClick={handleTap}></div>
+        </div>
+    );
+};
+
+export default Kitkat;
