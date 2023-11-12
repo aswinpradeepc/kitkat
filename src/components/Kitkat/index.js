c4f7482f323ea121ff3d495b2c5610c6951e05b8
aswinpradeepc <aswinpradeepc@gmail.com>
2023-11-12 08:47:42 +0530

    hehe punch perfect

--- src/components/Kitkat/index.js
+++ src/components/Kitkat/index.js
@@ -1,20 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import punch from './punch.mp3';
+import gifImage from './punchkill.gif';
 
 const Kitkat = () => {
     const [tapCount, setTapCount] = useState(0);
+    const [animationPosition, setAnimationPosition] = useState({ x: 0, y: 0 });
+    const [animationVisible, setAnimationVisible] = useState(false);
 
-    const handleTap = () => {
+    const handleTap = (event) => {
         playTapSound();
         setTapCount(tapCount + 1);
+        setAnimationVisible(true);
+
+        // Get the coordinates of the tap
+        const x = event.clientX;
+        const y = event.clientY;
+        setAnimationPosition({ x, y });
+
+        // Vibrate
         if ('vibrate' in navigator) {
             navigator.vibrate(200);
         }
+        setTimeout(() => {
+            setAnimationVisible(false)
+        }, 600);
     };
+
+    const animationStyle = {
+        position: 'absolute',
+        left: `${animationPosition.x}px`,
+        top: `${animationPosition.y}px`,
+    };
+
     const playTapSound = () => {
         const audio = new Audio(punch);
         audio.play();
     };
+
     const divStyle = {
         width: '100%',
         height: '100vh',
@@ -24,10 +46,17 @@ const Kitkat = () => {
 
     return (
         <div>
-            <div>
+            <div style={divStyle} onClick={handleTap}>
                 <h1>Kitkat Taps: {tapCount}</h1>
+                {animationVisible &&
+                    <img
+                        key={tapCount}
+                        src={gifImage}
+                        alt="Animation"
+                        style={animationStyle}
+                    />
+                }
             </div>
-            <div style={divStyle} onClick={handleTap}></div>
         </div>
     );
 };
