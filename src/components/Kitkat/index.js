c4f51ade25a16a5c89bb0a4d22b33fc665a6fc79
aswinpradeepc <aswinpradeepc@gmail.com>
2023-11-12 10:34:12 +0530

    final puzhhh

--- src/components/Kitkat/index.js
+++ src/components/Kitkat/index.js
@@ -1,11 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import punch from './punch.mp3';
 import gifImage from './punchkill.gif';
+import desktopImage from './desktop.jpeg'; // Replace with the actual path to your desktop image
+import mobileImage from './mobile.jpeg'; // Replace with the actual path to your mobile image
 
 const Kitkat = () => {
     const [tapCount, setTapCount] = useState(0);
     const [animationPosition, setAnimationPosition] = useState({ x: 0, y: 0 });
     const [animationVisible, setAnimationVisible] = useState(false);
+    const [backgroundImage, setBackgroundImage] = useState(desktopImage);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 768) {
+                setBackgroundImage(mobileImage);
+            } else {
+                setBackgroundImage(desktopImage);
+            }
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize(); // Call the function initially to set the background based on the initial window size
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     const handleTap = (event) => {
         playTapSound();
@@ -28,26 +48,30 @@ const Kitkat = () => {
 
     const animationStyle = {
         position: 'absolute',
-        left: `${animationPosition.x}px`,
-        top: `${animationPosition.y}px`,
+        left: `calc(${animationPosition.x}px - 10%)`,
+        top: `calc(${animationPosition.y}px - 10%)`,
     };
+
 
     const playTapSound = () => {
         const audio = new Audio(punch);
         audio.play();
     };
 
+    window.innerWidth < 768 ? document.body.style.color = 'white' : document.body.style.color = 'black';
+
     const divStyle = {
         width: '100%',
         height: '100vh',
-        backgroundColor: 'red',
         cursor: 'pointer',
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
     };
 
     return (
         <div>
             <div style={divStyle} onClick={handleTap}>
-                <h1>Kitkat Taps: {tapCount}</h1>
+                <h1 style={{ fontFamily: "'Pixelify Sans', sans-serif", fontSize: '4rem' }}>Punches: {tapCount}</h1>
                 {animationVisible &&
                     <img
                         key={tapCount}
