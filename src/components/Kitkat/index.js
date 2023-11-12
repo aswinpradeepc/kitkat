7192250e38193c7f1190aafc2969b715101a8b00
aswinpradeepc <aswinpradeepc@gmail.com>
2023-11-12 08:00:16 +0530

    hehe punch

--- src/components/Kitkat/index.js
+++ src/components/Kitkat/index.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
+import punch from './punch.mp3';
 
 const Kitkat = () => {
     const [tapCount, setTapCount] = useState(0);
 
     const handleTap = () => {
+        playTapSound();
         setTapCount(tapCount + 1);
         if ('vibrate' in navigator) {
             navigator.vibrate(200);
         }
     };
-
+    const playTapSound = () => {
+        const audio = new Audio(punch);
+        audio.play();
+    };
     const divStyle = {
         width: '100%',
         height: '100vh',
